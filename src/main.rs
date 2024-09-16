11e1f6e552256d9f9036e90212a663fb1debe498
John Freeman <prsjohnny@gmail.com>
2024-09-15 20:31:34 -0400

    installed clap

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 mod executor;
 mod request;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    request: String,
+    // #[arg(short, long, value_name = "FILE")]
+    // env: Option<String>,
+
+    // #[arg(short, long, action = clap::ArgAction::Count)]
+    // debug: u8,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let requests = request::load_requests_from_toml("requests.toml")?;
+    let cli = Cli::parse();
+
+    let requests = request::load_requests_from_toml(cli.request.as_str())?;
 
     executor::execute_request_chain(requests).await?;
 
