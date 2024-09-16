3e29efb4ce157c921e27894124d2af680db8585c
John Freeman <prsjohnny@gmail.com>
2024-09-15 20:34:17 -0400

    changed name from send to glint

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,9 @@
 use clap::Command;
 
 pub fn get_cli_matches() -> clap::ArgMatches {
-    Command::new("send")
+    Command::new("glint")
         .version("0.1.0")
-        .author("John Doe")
+        .author("John Freeman")
         .about("Send HTTP requests based on a request chain defined in TOML")
         .allow_external_subcommands(true)
         .get_matches()
