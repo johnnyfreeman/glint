80534b56dfb9ed3da6a5b5b3432b48594e737af6
John Freeman <prsjohnny@gmail.com>
2024-11-10 12:48:35 -0500

    more precise error bubbling

--- src/executor.rs
+++ src/executor.rs
@@ -14,9 +14,20 @@ use reqwest::Client;
 use serde_json::Value;
 use std::collections::HashMap;
 use std::sync::Mutex;
+use thiserror::Error;
 
 lazy_static! {
     static ref PLACEHOLDER_REGEX: Regex = Regex::new(r"\{(\w+)\}").unwrap();
+}
+
+#[derive(Error, Debug)]
+pub enum ExecutionError {
+    #[error("Request `{request:?}` was not found in history")]
+    RequestNotFound { request: String },
+    #[error("Template `{template:?}` could not be resolved")]
+    ResolutionFailed { template: String },
+    #[error("Unknown execution error")]
+    Unknown,
 }
 
 // Cache for loaded env files
@@ -48,7 +59,13 @@ impl Executor {
     pub async fn execute(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         match &self.options.request {
             Some(request) => {
-                let cloned_request = self.requests.get(request).expect("Invalid Request").clone();
+                let cloned_request = self
+                    .requests
+                    .get(request)
+                    .ok_or(ExecutionError::RequestNotFound {
+                        request: request.to_owned(),
+                    })?
+                    .clone();
                 self.execute_request(cloned_request).await?;
             }
             None => {
@@ -63,22 +80,41 @@ impl Executor {
     }
 
     #[tracing::instrument(skip(self))]
-    pub async fn execute_request(
-        &mut self,
-        request: Request,
-    ) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn execute_request(&mut self, request: Request) -> Result<(), ExecutionError> {
         // Resolve URL
-        let url =
-            resolve_placeholders(&request.url, &self.request_resolver, &request.dependencies)?;
+        let url = self
+            .resolve_placeholders(&request.url, &request.dependencies)
+            .await
+            .map_err(|_| ExecutionError::ResolutionFailed {
+                template: request.url,
+            })?;
         let headers = if let Some(header_map) = &request.headers {
             let mut resolved_headers = HeaderMap::new();
             for (key, value) in header_map {
-                let resolved_key =
-                    resolve_placeholders(key, &self.request_resolver, &request.dependencies)?;
-                let resolved_value =
-                    resolve_placeholders(value, &self.request_resolver, &request.dependencies)?;
-                let header_name = HeaderName::from_bytes(resolved_key.as_bytes())?;
-                resolved_headers.insert(header_name, resolved_value.parse()?);
+                let resolved_key = self
+                    .resolve_placeholders(key, &request.dependencies)
+                    .await
+                    .map_err(|_| ExecutionError::ResolutionFailed {
+                        template: key.to_owned(),
+                    })?;
+                let resolved_value = self
+                    .resolve_placeholders(value, &request.dependencies)
+                    .await
+                    .map_err(|_| ExecutionError::ResolutionFailed {
+                        template: value.to_owned(),
+                    })?;
+                let header_name =
+                    HeaderName::from_bytes(resolved_key.as_bytes()).map_err(|_| {
+                        ExecutionError::ResolutionFailed {
+                            template: value.to_owned(),
+                        }
+                    })?;
+                resolved_headers.insert(
+                    header_name,
+                    resolved_value
+                        .parse()
+                        .map_err(|_| ExecutionError::Unknown)?,
+                );
             }
             resolved_headers
         } else {
@@ -87,11 +123,11 @@ impl Executor {
 
         // Resolve the request body, if it exists
         let body = if let Some(body_template) = &request.body {
-            Some(resolve_placeholders(
-                body_template,
-                &self.request_resolver,
-                &request.dependencies,
-            )?)
+            Some(
+                self.resolve_placeholders(body_template, &request.dependencies)
+                    .await
+                    .map_err(|_| ExecutionError::Unknown)?,
+            )
         } else {
             None
         };
@@ -100,20 +136,22 @@ impl Executor {
         let res = self
             .http
             .request(
-                reqwest::Method::from_bytes(request.method.as_bytes())?,
+                reqwest::Method::from_bytes(request.method.as_bytes())
+                    .map_err(|_| ExecutionError::Unknown)?,
                 &url,
             )
             .headers(headers)
             .body(body.unwrap_or_default())
             .send()
-            .await?;
+            .await
+            .map_err(|_| ExecutionError::Unknown)?;
 
         // Extract the headers first, since `res.text()` will consume `res`
         let headers: HeaderMap = res.headers().clone(); // Clone the headers to avoid borrowing issues
 
         // Now you can safely consume the body
         let status = res.status();
-        let body_text = res.text().await?;
+        let body_text = res.text().await.map_err(|_| ExecutionError::Unknown)?;
 
         println!(
             "\n{} {}",
@@ -151,26 +189,30 @@ impl Executor {
             PrettyPrinter::new()
                 .input_from_bytes(headers_formatted.as_bytes()) // Use the formatted headers
                 .language("toml") // Print as TOML (or use "yaml" for a similar format)
-                .print()?;
+                .print()
+                .map_err(|_| ExecutionError::Unknown)?;
         }
 
         println!();
 
         // Pretty print the body, if it is valid JSON
-        if let Ok(pretty_json) =
-            serde_json::to_string_pretty(&serde_json::from_str::<serde_json::Value>(&body_text)?)
-        {
+        if let Ok(pretty_json) = serde_json::to_string_pretty(
+            &serde_json::from_str::<serde_json::Value>(&body_text)
+                .map_err(|_| ExecutionError::Unknown)?,
+        ) {
             PrettyPrinter::new()
                 .input_from_bytes(pretty_json.as_bytes()) // Use the formatted pretty JSON
                 .language("json") // Specify JSON language for highlighting
-                .print()?;
+                .print()
+                .map_err(|_| ExecutionError::Unknown)?;
             println!();
         } else {
             // If it's not JSON, print the raw body as plain text
             PrettyPrinter::new()
                 .input_from_bytes(body_text.as_bytes()) // Use raw body text
                 .language("plain") // Print as plain text
-                .print()?;
+                .print()
+                .map_err(|_| ExecutionError::Unknown)?;
             println!();
         }
 
@@ -183,88 +225,106 @@ impl Executor {
 
         Ok(())
     }
-}
 
-// TODO: replace below
+    #[tracing::instrument(skip(self))]
+    async fn resolve_placeholders(
+        &mut self,
+        template: &str,
+        request_dependencies: &Option<HashMap<String, Dependency>>,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        let mut resolved = template.to_string();
 
-#[tracing::instrument(skip(request_resolver))]
-fn resolve_placeholders(
-    template: &str,
-    request_resolver: &RequestResolver,
-    request_dependencies: &Option<HashMap<String, Dependency>>,
-) -> Result<String, Box<dyn std::error::Error>> {
-    let mut resolved = template.to_string();
+        // Find all placeholders in the template
+        for caps in PLACEHOLDER_REGEX.captures_iter(template) {
+            let placeholder = &caps[1]; // The name inside {}
 
-    // Find all placeholders in the template
-    for caps in PLACEHOLDER_REGEX.captures_iter(template) {
-        let placeholder = &caps[1]; // The name inside {}
+            // Try to resolve the placeholder
+            let value = if let Some(dep) = request_dependencies
+                .as_ref()
+                .and_then(|deps| deps.get(placeholder))
+            {
+                self.resolve_dependency_value(dep, placeholder).await?
+            } else {
+                return Err(format!("Unable to resolve placeholder: {}", placeholder).into());
+            };
 
-        // Try to resolve the placeholder
-        let value = if let Some(dep) = request_dependencies
-            .as_ref()
-            .and_then(|deps| deps.get(placeholder))
-        {
-            resolve_dependency_value(dep, placeholder, request_resolver)?
-        } else {
-            return Err(format!("Unable to resolve placeholder: {}", placeholder).into());
-        };
+            // Replace the placeholder in the resolved string
+            resolved = resolved.replace(&format!("{{{}}}", placeholder), &value);
+        }
 
-        // Replace the placeholder in the resolved string
-        resolved = resolved.replace(&format!("{{{}}}", placeholder), &value);
+        Ok(resolved)
     }
 
-    Ok(resolved)
-}
+    #[tracing::instrument(skip(self))]
+    async fn resolve_dependency_value(
+        &mut self,
+        dep: &Dependency,
+        _placeholder: &str,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        match dep {
+            Dependency::EnvFile {
+                env_file,
+                key,
+                prompt,
+            } => {
+                // Load the TOML file
+                let mut env_data = load_env_file(env_file)?;
 
-#[tracing::instrument(skip(request_resolver))]
-fn resolve_dependency_value(
-    dep: &Dependency,
-    _placeholder: &str,
-    request_resolver: &RequestResolver,
-) -> Result<String, Box<dyn std::error::Error>> {
-    match dep {
-        Dependency::EnvFile {
-            env_file,
-            key,
-            prompt,
-        } => {
-            // Load the TOML file
-            let mut env_data = load_env_file(env_file)?;
+                if let Some(value) = env_data.get(key) {
+                    Ok(value.clone())
+                } else if let Some(prompt) = prompt {
+                    // Prompt the user
+                    let value = prompt_user(prompt);
+                    // Optionally, save the value back to the env file or cache
+                    env_data.insert(key.clone(), value.clone());
+                    // Save back to the file
+                    save_env_file(env_file, &env_data)?;
+                    // Update the cache
+                    let mut cache = ENV_FILES_CACHE.lock().unwrap();
+                    cache.insert(env_file.clone(), env_data);
+                    Ok(value)
+                } else {
+                    Err(format!("Key '{}' not found in file '{}'", key, env_file).into())
+                }
+            }
+            Dependency::EnvVar { name, prompt } => {
+                if let Ok(env_value) = std::env::var(name) {
+                    Ok(env_value)
+                } else if let Some(prompt) = prompt {
+                    Ok(prompt_user(prompt))
+                } else {
+                    Err(format!("Environment variable '{}' not found", name).into())
+                }
+            }
+            Dependency::File { path } => {
+                let file_content = std::fs::read_to_string(path)?;
+                Ok(file_content.trim().to_string())
+            }
+            Dependency::Request { request, path } => {
+                // TODO: Resolve fresh request if this fails
+                match self
+                    .request_resolver
+                    .resolve((request.to_owned(), path.to_owned()))
+                {
+                    Ok(value) => Ok(value),
+                    Err(_) => {
+                        let cloned_request = self
+                            .requests
+                            .get(request)
+                            .ok_or(ExecutionError::RequestNotFound {
+                                request: request.to_owned(),
+                            })?
+                            .clone();
+                        Box::pin(self.execute_request(cloned_request)).await?;
 
-            if let Some(value) = env_data.get(key) {
-                Ok(value.clone())
-            } else if let Some(prompt) = prompt {
-                // Prompt the user
-                let value = prompt_user(prompt);
-                // Optionally, save the value back to the env file or cache
-                env_data.insert(key.clone(), value.clone());
-                // Save back to the file
-                save_env_file(env_file, &env_data)?;
-                // Update the cache
-                let mut cache = ENV_FILES_CACHE.lock().unwrap();
-                cache.insert(env_file.clone(), env_data);
-                Ok(value)
-            } else {
-                Err(format!("Key '{}' not found in file '{}'", key, env_file).into())
+                        Ok(self
+                            .request_resolver
+                            .resolve((request.to_owned(), path.to_owned()))?)
+                    }
+                }
             }
+            Dependency::Prompt { label } => Ok(prompt_user(label)),
         }
-        Dependency::EnvVar { name, prompt } => {
-            if let Ok(env_value) = std::env::var(name) {
-                Ok(env_value)
-            } else if let Some(prompt) = prompt {
-                Ok(prompt_user(prompt))
-            } else {
-                Err(format!("Environment variable '{}' not found", name).into())
-            }
-        }
-        Dependency::File { path } => {
-            let file_content = std::fs::read_to_string(path)?;
-            Ok(file_content.trim().to_string())
-        }
-        Dependency::Request { request, path } => {
-            Ok(request_resolver.resolve((request.to_owned(), path.to_owned()))?)
-        }
-        Dependency::Prompt { label } => Ok(prompt_user(label)),
     }
 }
 
